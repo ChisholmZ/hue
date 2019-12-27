3469acc67dc10459e9e48272c1566a85c035098a
Zach <ChisholmZ@Schneider.com>
2019-12-27 15:51:51 -0600

    use python3

--- disco.py
+++ disco.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 import argparse, random
 from phue import Bridge
 from time import sleep
--- xmas.py
+++ xmas.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 import argparse
 from phue import Bridge
 from time import sleep
